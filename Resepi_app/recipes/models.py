fba20b971af635e83641c89855de50ba69e5e63b
baby6969 <yafetzekarias23@fmail.com>
2025-10-12 05:14:36 -0400

    update recipe/models.py

--- Resepi_app/recipes/models.py
+++ Resepi_app/recipes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category (models.Model):
+    name = models.CharField (max_length = 100)
+  
+    
+    def __str__(self):
+     return self.name
+
+
+class Ingredient(models.Model):
+  name = models.CharField (max_length= 100)
+  
+  def __str__(self):
+    return self.name
+
+class Recipe (models.Model):
+    user  = models.ForeignKey(User, on_delete=models.CASCADE,related_name="recipes")
+    title =models.CharField (max_length =200)
+    description = models.TextField()
+    instructions =models.TextField()
+    categories =models.ManyToManyField(Category, related_name="recipes")
+    ingredients =models.ManyToManyField(Ingredient, related_name="recipes")
+    created_at = models.DateTimeField(auto_now_add=True)
